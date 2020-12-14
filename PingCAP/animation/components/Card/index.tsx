210e7d57b2529cc2b63ebc69f9c1ecc205931301
caohiulin <1057275848@qq.com>
2020-12-14 20:44:40 +0800

    定义active state

--- PingCAP/animation/components/Card/index.tsx
+++ PingCAP/animation/components/Card/index.tsx
@@ -1,19 +1,57 @@
+import { useEffect, useRef, useState } from "react";
 import { ICardItem } from "../../constants/cards";
 import styles from "./style.module.scss";
 
-type ICardProps = ICardItem & {};
+type ICardProps = ICardItem & {
+  isActive: boolean;
+  onActive: () => void;
+};
 export default function Card(props: ICardProps) {
-  const { title, desc, img } = props;
+  const { title, desc, img, isActive, onActive } = props;
+
+  const itemElement = useRef<HTMLDivElement>(null);
+  const [size, setSize] = useState<[number, number]>([0, 0]);
+  const [scale, setScale] = useState<[number, number]>([1, 1]);
+
+  useEffect(() => {
+    const dom = itemElement.current!;
+    const width = dom.clientWidth;
+    const height = dom.clientHeight;
+    setSize([width, height]);
+  }, []);
+  useEffect(() => {
+    if (isActive) {
+      // 滚动条的高度 20px
+      const winWidth = window.outerWidth + 20;
+      const winHeight = window.outerHeight + 20;
+      const sx = winWidth / size[0];
+      const sy = winHeight / size[1];
+      setScale([sx, sy]);
+    } else {
+      setScale([1, 1]);
+    }
+  }, [isActive]);
   return (
-    <div className={styles.card}>
-      <figure>
+    <div className={styles.card} onClick={onActive}>
+      <div
+        ref={itemElement}
+        className={`${styles.item} ${isActive ? styles.active : ""}`}
+        style={{
+          width: size[0] || "inhert",
+          height: size[1] || "inhert",
+          transform: `scale(${scale[0]}, ${scale[1]})`,
+        }}
+      >
         <img className={styles.img} src={img} alt="title" />
-        <div className={styles.background}></div>
-        <figcaption>
-          <div className={styles.title}>{title}</div>
-          <p>{desc}</p>
-        </figcaption>
-      </figure>
+        {/* <figure>
+          <img className={styles.img} src={img} alt="title" />
+          <div className={styles.background}></div>
+          <figcaption>
+            <div className={styles.title}>{title}</div>
+            <p>{desc}</p>
+          </figcaption>
+        </figure> */}
+      </div>
     </div>
   );
 }
--- PingCAP/animation/pages/index.tsx
+++ PingCAP/animation/pages/index.tsx
@@ -1,17 +1,28 @@
 import Head from "next/head";
+import { useState } from "react";
 import Card from "../components/Card";
 import { Cards } from "../constants/cards";
 
 import style from "./style.module.scss";
 
 export default function Home() {
+  const [active, setActive] = useState<number | null>(null);
+
+  const handleActive = (index: number) => {
+    setActive(index);
+  };
   return (
     <div className={style.home}>
       <Head>
         <title>animation</title>
       </Head>
       {Cards.map((card, index) => (
-        <Card key={index} {...card} />
+        <Card
+          key={index}
+          {...card}
+          isActive={active === index}
+          onActive={() => handleActive(index)}
+        />
       ))}
     </div>
   );
