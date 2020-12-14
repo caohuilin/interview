55d7c7ba32cd9db2244a711e7315b099798a8d0e
caohiulin <1057275848@qq.com>
2020-12-14 16:16:04 +0800

    调整项目结构 && init animation

---
+++ PingCAP/animation/README.md
@@ -0,0 +1,5 @@
+# 题目一 设计还原
+
+实现下方 GIF 展示的形变效果，可以做适当的抽象和可复用性。
+
+<image src="https://img.pingcap.com/fe-hire/3-demo.gif" />
---
+++ PingCAP/animation/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>animation</title>
+      </Head>
+      Home
+    </div>
+  );
+}
