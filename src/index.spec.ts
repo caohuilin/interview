6ae4ce145bb3be316ca9a095812ac6d8d9fcf47d
caohiulin <1057275848@qq.com>
2020-12-14 14:54:34 +0800

    reset demo

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,17 +1,9 @@
-type ParamType<T> = T extends (param: infer P) => any ? P : T;
-
-type Caller<T extends (...args: any[]) => any> = () => ParamType<T>;
-
-function tx(x: string) {
-  return x;
+function sum(a: number, b: number) {
+  return a + b;
 }
-
-const caller: Caller<typeof tx> = () => {
-  return "x";
-};
 
 describe("two sum", () => {
   it("1 + 2", () => {
-    expect(caller()).toEqual("x");
+    expect(sum(1, 2)).toBe(3);
   });
 });
